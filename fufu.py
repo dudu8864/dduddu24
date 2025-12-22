f86954bd0b0734864e7579390762ed016af21f29
dudu <aannyeong544@gmail.com>
2025-12-22 16:29:38 +0900

    sdsdsdsdsd

--- fufu.py
+++ fufu.py
@@ -1,3 +1,4 @@
+
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 
@@ -5,44 +6,98 @@ import time
 
 app = Ursina()
 
-player = FirstPersonController(collider = 'box')
+#EditorCamera()
+player = FirstPersonController( 
+    model = 'sphere',
+    scale = 1, 
+    color = (0,0,0,0),
+    speed = 15)
 
 
 ball = Entity(
     parent=camera,
     model='sphere',
     color=color.red,
-    scale=0.6,
+    scale=0.4,
     collider='sphere',
-    position=(0.4, -0.3, 0.7)
+    position=(-10,0,0) 
 )
+ball.undestroyable = True
 
-ground = Entity(model='plane', scale=(50,1,50), color=color.white.tint(-0.2), collider='box')
+ground = Entity(
+    model='plane',
+    collider='box',
+    scale=64,
+    texture='grass',
+    texture_scale=(4,4)
+)
+ground.undestroyable = True
 
-
-player.jump_height = 3
 mouse.locked = True
 window.fullscreen = True
 camera.fov = 90
 blocks = []
+jump_cooltime = 0
+jump_delay = 0.8  # 0.3초마다 점프
+gravity = 0.98
+state = 0 # 쏘기전
+ball.direction = Vec3(0,0,0)
+
+def update():
+    global state
+    #print(f'플레이어포워드값 {player.forward}')
+    #print(f'플레이어 위치 {player.world_position}')
+    global jump_cooltime
+
+    if jump_cooltime > 0:
+        jump_cooltime -= time.dt
+
+    if held_keys['space'] and jump_cooltime <= 0 and player.grounded:
+        player.jump()
+        jump_cooltime = jump_delay
+
+        player.camera_pivot.rotation_y = clamp(player.camera_pivot.rotation_y,-80,80)
+
+    if state == 1:
+        speed = 40
+        move_step = ball.direction * speed * time.dt
+        ball.direction.y -= 0.5 * time.dt
+        hit_info = raycast(ball.world_position, ball.direction, distance=move_step.length(), ignore=(ball, player))
+
+        if hit_info.hit:
+            ball.position = hit_info.world_point
+            #ball.position += hit_info.normal * (ball.scale.x / 2)
+            state = 0 
+        else:
+            ball.position += move_step
+            
+            ball.direction.y -= gravity * time.dt
+
+
+class Voxel(Button):
+    def __init__(self, position=(0,0,0)):
+        super().__init__(
+            parent=scene,
+            position=position,
+            model='cube',
+            origin_y=.10,
+            texture='white_cube',
+            color=color.hsv(0, 0, random.uniform(.9, 1.0)),
+            highlight_color=color.lime,
+        )
 
 
 def input(key):
-    global blocks
-    if ground.hovered:
-        if key == 'left mouse down':
-            block = Entity(model='cube',color=color.red.tint(0.4),texture='brick', scale=(2,2,2),world_position=mouse.world_point, collider = 'box' )
-            blocks.append(block)
-            print('wa sans!')
-            
-    for block in blocks:
-            if block.hovered == True: 
-                if key == 'left mouse down': 
-                    block = Entity(model='cube',color=color.red.tint(.4), texture='brick',scale=(2,2,2),position=mouse.world_point,collider = 'box' )
-                    blocks.append(block)
-                if key == 'right mouse down':
-                    blocks.remove(block)
-                    destroy(block)
+    global state
+    if key == 'left mouse down':
+        hit_info = raycast(camera.world_position, camera.forward, distance=5)
+        if hit_info.hit:
+            Voxel(position=hit_info.entity.position + hit_info.normal)
+    if key == 'right mouse down':
+        e = mouse.hovered_entity
+        if e and not getattr(e, 'undestroyable', False):
+            destroy(e)
+
 
     if key == 'escape':
         application.quit()
@@ -52,15 +107,20 @@ def input(key):
 
     if key == 'e':
         if ball.parent == camera:
+            state = 1
             ball.parent = scene
-            ball.position = player.world_position + player.forward * 1 + Vec3(0, 0.5, 0)
-            if ball.intersects().hit:
-                ball.position -= player.forward * 0.1
-            else:
-                ball.velocity = player.forward * 20
+            ball.scale = 1
+            ball.position = camera.world_position + camera.forward
+            ball.direction = camera.forward
         else:
+            state = 0
             ball.parent = camera
             ball.position = Vec3(0.5, -0.3, 0.8)
+            ball.scale = 0.3
+
+for z in range(10):
+    for x in range(10):
+        voxel = Voxel(position=(x+5,1,z))
 
 app.run()
 
